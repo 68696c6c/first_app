39a7102a4918a11618319798f0e656ba6f0964cd
root <root@pungentnoise.com>
2011-01-09 14:49:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a2dd4e77e4570dd7e09aa5961c4fb5c60a5887d3b69da2df06f1e8984d5dc7d94139915aaaf14facd415087d969c706903ad08a8d8715bfc1ead9eb5535a16d0'
